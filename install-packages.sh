b1d54ee3759382f29fc7257b85fe9120b896f7f7
ChrisKelton <cbliminal97@gmail.com>
2024-02-15 20:23:49 -0500

    clean up repo and utilize setup.py files for installing modules more easily.
    improve download script to automate it entirely without requiring download directories and make consistent where the data will be downloaded.
    
    add .gitignore to not upload the data nor cityscapeScripts repo.

---
+++ install-packages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+is_conda_env_active="$(conda info --envs | grep -F "*" | wc -l)"
+if [ $is_conda_env_active -lt 1 ]; then
+	echo "Please activate your conda environment before invoking this script"
+	exit 125
+fi
+current_dir="$(cd $(dirname $0) && pwd)"
+pip install -e $current_dir/utils
+pip install -e $current_dir/data_prep
